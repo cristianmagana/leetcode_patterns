248a799e9a16fcf547cc9f8f9508ec6e189d3092
Cristian Magana <cmagana@email.com>
2022-12-31 09:53:28 -0800

    sliding_window

--- 01_sliding_window/algorithm.ts
+++ 01_sliding_window/algorithm.ts
@@ -1,19 +1,25 @@
 const slidingWindow = (nums: number[]): number => {
 	
+	let result = 0;
+
 	// how many changes available?
 	// digits flipped/removed etc
-	int k = 0;
+	let k = 0;
 
 	// slow pointer
-	int i = 0
+	let i = 0
 
+	// fast pointer 
 	for(let j = 0; j < nums.length; j++) {
 		
+		// checks if fast pointer encounters a 0. 
+		// if so decrement threshold value.
 		if(nums[j] == 0) {
 			k--;
 		}
+
+		// moves window to right
 		while(k < 0) {
-		
 			if(nums[i] == 0) {
 				k++;
 			}	
